042559469c9b61c62ea48a81b7a4c1e07382fbe5
Shamiksha <silkyraj23@gmail.com>
2025-02-22 00:19:54 +0530

    [Shamiksha] add: Spring Messanging App Project GET Request Method and pass name as path variable feature added✨

--- springmessagingapp/src/main/java/com/example/springmessagingapp/controller/MessagingRestController.java
+++ springmessagingapp/src/main/java/com/example/springmessagingapp/controller/MessagingRestController.java
@@ -1,9 +1,6 @@
 package com.example.springmessagingapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -18,6 +15,9 @@ public class MessagingRestController {
         return  "Hello " + name +" , Welcome to BridgeLabz!";
     }
 
-
+    @GetMapping("/{name}")
+    public String hello3(@PathVariable String name){
+        return  "Hello " + name +" , Welcome to BridgeLabz!";
+    }
 
 }
